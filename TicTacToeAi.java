ed05eebd17b16d14550b990920dfe2a7e9c4340e
SayuriYuto <dhuriamey02@gmail.com>
2022-10-03 12:08:11 +0530

    removed spaces

--- TicTacToeAi.java
+++ TicTacToeAi.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 import java.util.HashSet;
 
@@ -12,7 +11,7 @@ class TicTacToeAi {
         board = new Board();
     }
 
-    private void play () {
+    private void play() {
 
         System.out.println("Lets play!");
 
@@ -30,7 +29,7 @@ class TicTacToeAi {
         }
     }
 
-    private void playMove () {
+    private void playMove() {
         if (board.getTurn() == Board.State.X) { // X is you
             getPlayerMove();
         } else {
@@ -38,12 +37,12 @@ class TicTacToeAi {
         }
     }
 
-    private void printGameStatus () {
+    private void printGameStatus() {
         System.out.println("\n" + board + "\n");
         System.out.println(board.getTurn().name() + " its your turn!");
     }
 
-    private void getPlayerMove () {
+    private void getPlayerMove() {
         System.out.print("Index of move: ");
 
         int move = sc.nextInt();
@@ -54,7 +53,7 @@ class TicTacToeAi {
         } else {
             move = move + 5;
         }
-        if (move < 0 || move >= Board.BOARD_WIDTH* Board.BOARD_WIDTH) {
+        if (move < 0 || move >= Board.BOARD_WIDTH * Board.BOARD_WIDTH) {
             System.out.println("\nInvalid move.");
             System.out.println("\nThe index of the move must be between 0 and "
                     + (Board.BOARD_WIDTH * Board.BOARD_WIDTH - 1) + ", inclusive.");
@@ -64,7 +63,7 @@ class TicTacToeAi {
         }
     }
 
-    private void printWinner () {
+    private void printWinner() {
         Board.State winner = board.getWinner();
 
         System.out.println("\n" + board + "\n");
@@ -76,18 +75,17 @@ class TicTacToeAi {
         }
     }
 
-    private boolean tryAgain () {
+    private boolean tryAgain() {
         if (promptTryAgain()) {
             board.reset();
             System.out.println("Started new game.");
             System.out.println("X's turn.");
             return true;
         }
-
         return false;
     }
 
-    private boolean promptTryAgain () {
+    private boolean promptTryAgain() {
         while (true) {
             System.out.print("Would you like to start a new game? (Y/N): ");
             String response = sc.next();
@@ -104,14 +102,14 @@ class TicTacToeAi {
         TicTacToeAi ticTacToe = new TicTacToeAi();
         ticTacToe.play();
     }
-
 }
 
 class MiniMax {
 
     private static double maxPly;
 
-    private MiniMax() {}
+    private MiniMax() {
+    }
 
     static void run(Board.State player, Board board, double maxPly) {
         if (maxPly < 1) {
@@ -122,7 +120,7 @@ class MiniMax {
     }
 
     private static int miniMax(Board.State player, Board board, int currentPly) {
-        if (currentPly++ == maxPly || board.isGameOver()) {   //base case
+        if (currentPly++ == maxPly || board.isGameOver()) { // base case
             return score(player, board);
         }
         if (board.getTurn() == player) {
@@ -130,27 +128,20 @@ class MiniMax {
         } else {
             return getMin(player, board, currentPly);
         }
-
     }
 
     private static int getMax(Board.State player, Board board, int currentPly) {
         double bestScore = Double.NEGATIVE_INFINITY;
         int indexOfBestMove = -1;
-
         for (Integer theMove : board.getAvailableMoves()) {
-
             Board modifiedBoard = board.getDeepCopy();
             modifiedBoard.move(theMove);
-
             int score = miniMax(player, modifiedBoard, currentPly);
-
             if (score >= bestScore) {
                 bestScore = score;
                 indexOfBestMove = theMove;
             }
-
         }
-
         board.move(indexOfBestMove);
         return (int) bestScore;
     }
@@ -160,19 +151,14 @@ class MiniMax {
         int indexOfBestMove = -1;
 
         for (Integer theMove : board.getAvailableMoves()) {
-
             Board modifiedBoard = board.getDeepCopy();
             modifiedBoard.move(theMove);
-
             int score = miniMax(player, modifiedBoard, currentPly);
-
             if (score <= bestScore) {
                 bestScore = score;
                 indexOfBestMove = theMove;
             }
-
         }
-
         board.move(indexOfBestMove);
         return (int) bestScore;
     }
@@ -191,17 +177,18 @@ class MiniMax {
 
 }
 
-
 class Board {
 
     static final int BOARD_WIDTH = 3;
 
-    public enum State {Blank, X, O}
+    public enum State {
+        Blank, X, O
+    }
+
     private State[][] board;
     private State playersTurn;
     private State winner;
     private HashSet<Integer> movesAvailable;
-
     private int moveCount;
     private boolean gameOver;
 
@@ -211,21 +198,19 @@ class Board {
         reset();
     }
 
-    private void initialize () {
+    private void initialize() {
         for (int row = 0; row < BOARD_WIDTH; row++) {
             for (int col = 0; col < BOARD_WIDTH; col++) {
                 board[row][col] = State.Blank;
             }
         }
-
         movesAvailable.clear();
-
-        for (int i = 0; i < BOARD_WIDTH*BOARD_WIDTH; i++) {
+        for (int i = 0; i < BOARD_WIDTH * BOARD_WIDTH; i++) {
             movesAvailable.add(i);
         }
     }
 
-    void reset () {
+    void reset() {
         moveCount = 0;
         gameOver = false;
         playersTurn = State.X;
@@ -233,11 +218,11 @@ class Board {
         initialize();
     }
 
-    public boolean move (int index) {
-        return move(index% BOARD_WIDTH, index/ BOARD_WIDTH);
+    public boolean move(int index) {
+        return move(index % BOARD_WIDTH, index / BOARD_WIDTH);
     }
 
-    private boolean move (int x, int y) {
+    private boolean move(int x, int y) {
 
         if (board[y][x] == State.Blank) {
             board[y][x] = playersTurn;
@@ -264,57 +249,57 @@ class Board {
         return true;
     }
 
-    public boolean isGameOver () {
+    public boolean isGameOver() {
         return gameOver;
     }
 
-    State[][] toArray () {
+    State[][] toArray() {
         return board.clone();
     }
 
-    public State getTurn () {
+    public State getTurn() {
         return playersTurn;
     }
 
-    public State getWinner () {
+    public State getWinner() {
         return winner;
     }
 
-    public HashSet<Integer> getAvailableMoves () {
+    public HashSet<Integer> getAvailableMoves() {
         return movesAvailable;
     }
 
-    private void checkRow (int row) {
+    private void checkRow(int row) {
         for (int i = 1; i < BOARD_WIDTH; i++) {
-            if (board[row][i] != board[row][i-1]) {
+            if (board[row][i] != board[row][i - 1]) {
                 break;
             }
-            if (i == BOARD_WIDTH -1) {
+            if (i == BOARD_WIDTH - 1) {
                 winner = playersTurn;
                 gameOver = true;
             }
         }
     }
 
-    private void checkColumn (int column) {
+    private void checkColumn(int column) {
         for (int i = 1; i < BOARD_WIDTH; i++) {
-            if (board[i][column] != board[i-1][column]) {
+            if (board[i][column] != board[i - 1][column]) {
                 break;
             }
-            if (i == BOARD_WIDTH -1) {
+            if (i == BOARD_WIDTH - 1) {
                 winner = playersTurn;
                 gameOver = true;
             }
         }
     }
 
-    private void checkDiagonalFromTopLeft (int x, int y) {
+    private void checkDiagonalFromTopLeft(int x, int y) {
         if (x == y) {
             for (int i = 1; i < BOARD_WIDTH; i++) {
-                if (board[i][i] != board[i-1][i-1]) {
+                if (board[i][i] != board[i - 1][i - 1]) {
                     break;
                 }
-                if (i == BOARD_WIDTH -1) {
+                if (i == BOARD_WIDTH - 1) {
                     winner = playersTurn;
                     gameOver = true;
                 }
@@ -322,13 +307,13 @@ class Board {
         }
     }
 
-    private void checkDiagonalFromTopRight (int x, int y) {
-        if (BOARD_WIDTH -1-x == y) {
+    private void checkDiagonalFromTopRight(int x, int y) {
+        if (BOARD_WIDTH - 1 - x == y) {
             for (int i = 1; i < BOARD_WIDTH; i++) {
-                if (board[BOARD_WIDTH -1-i][i] != board[BOARD_WIDTH -i][i-1]) {
+                if (board[BOARD_WIDTH - 1 - i][i] != board[BOARD_WIDTH - i][i - 1]) {
                     break;
                 }
-                if (i == BOARD_WIDTH -1) {
+                if (i == BOARD_WIDTH - 1) {
                     winner = playersTurn;
                     gameOver = true;
                 }
@@ -336,26 +321,23 @@ class Board {
         }
     }
 
-    public Board getDeepCopy () {
-        Board board             = new Board();
-
+    public Board getDeepCopy() {
+        Board board = new Board();
         for (int i = 0; i < board.board.length; i++) {
             board.board[i] = this.board[i].clone();
         }
-
-        board.playersTurn       = this.playersTurn;
-        board.winner            = this.winner;
-        board.movesAvailable    = new HashSet<>();
+        board.playersTurn = this.playersTurn;
+        board.winner = this.winner;
+        board.movesAvailable = new HashSet<>();
         board.movesAvailable.addAll(this.movesAvailable);
-        board.moveCount         = this.moveCount;
-        board.gameOver          = this.gameOver;
+        board.moveCount = this.moveCount;
+        board.gameOver = this.gameOver;
         return board;
     }
 
     @Override
-    public String toString () {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-
         for (int y = 0; y < BOARD_WIDTH; y++) {
             for (int x = 0; x < BOARD_WIDTH; x++) {
 
@@ -365,14 +347,11 @@ class Board {
                     sb.append(board[y][x].name());
                 }
                 sb.append(" ");
-
             }
-            if (y != BOARD_WIDTH -1) {
+            if (y != BOARD_WIDTH - 1) {
                 sb.append("\n");
             }
         }
-
         return new String(sb);
     }
-
 }
